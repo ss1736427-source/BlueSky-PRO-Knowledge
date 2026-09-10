2e1ca216eba7612495d4529a6901454f2dddd601
ss1736427-source <ss1736427@gmail.com>
2026-09-10 23:26:59 +0300

    Align A* with normalized planning graph

--- 04_SOFTWARE/PLANNING/solvers/astar_solver.hpp
+++ 04_SOFTWARE/PLANNING/solvers/astar_solver.hpp
@@ -1,37 +1,20 @@
 #pragma once
 
+#include "../model/planning_graph.hpp"
 #include "../solver_contract/solver_contract.hpp"
 
 #include <atomic>
 #include <functional>
 #include <string>
-#include <unordered_map>
 #include <vector>
 
 namespace bluesky::planning {
 
-struct AStarNode {
-    std::string id;
-    double g{0.0};
-    double h{0.0};
-    std::string parent;
-};
-
-struct AStarEdge {
-    std::string from;
-    std::string to;
-    double cost{0.0};
-};
-
 class AStarSolver final : public Solver {
 public:
-    using Heuristic = std::function<double(const std::string&, const std::string&)>;
-    using GoalTest = std::function<bool(const std::string&)>;
+    using Heuristic = std::function<double(const PlanningNode&, const PlanningNode&)>;
 
-    AStarSolver(std::vector<AStarNode> nodes,
-                std::vector<AStarEdge> edges,
-                Heuristic heuristic,
-                GoalTest goal_test);
+    explicit AStarSolver(Heuristic heuristic);
 
     SolverMetadata metadata() const override;
     bool eligible(const MissionProblem& problem) const override;
@@ -39,10 +22,7 @@ public:
     void cancel() override;
 
 private:
-    std::vector<AStarNode> nodes_;
-    std::vector<AStarEdge> edges_;
     Heuristic heuristic_;
-    GoalTest goal_test_;
     std::atomic<bool> cancelled_{false};
 };
 
