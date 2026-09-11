a696d241bf6c7822bf4bab6a89d6cef04ef7f573
ss1736427-source <ss1736427@gmail.com>
2026-09-11 15:52:56 +0300

    Set orchestrator polling to one second

--- 00_PROJECT/EXTERNAL_ORCHESTRATOR/run_orchestrator.ps1
+++ 00_PROJECT/EXTERNAL_ORCHESTRATOR/run_orchestrator.ps1
@@ -8,7 +8,7 @@ $ErrorActionPreference = "Stop"
 
 $env:BS_REPO = $Repo
 $env:BS_BRANCH = "main"
-$env:BS_POLL_SECONDS = "5"
+$env:BS_POLL_SECONDS = "1"
 $env:BS_CI_GRACE_SECONDS = "5"
 if ($AgentExecutable) { $env:BS_AGENT_EXECUTABLE = $AgentExecutable }
 if ($AgentArguments) { $env:BS_AGENT_ARGUMENTS = $AgentArguments }
