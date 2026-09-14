5cfffcd868e94e79ca5420f10bf495c0eaad9fa3
ss1736427-source <ss1736427@gmail.com>
2026-09-14 20:28:24 +0300

    EC-05: add Mission Compiler contract

---
+++ 04_SOFTWARE/PLANNING/compiler/mission_compiler.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "../model/mission_model.hpp"
+
+namespace bluesky::planning {
+
+struct ExecutableMissionPackage {
+    std::string package_id;
+    std::string vehicle_id;
+    std::string package_version;
+    std::string mission_id;
+    std::string mission_version;
+    std::vector<std::string> execution_steps;
+};
+
+class MissionCompiler {
+public:
+    static bool compile(const Mission& mission,
+                        ExecutableMissionPackage& package,
+                        std::string& rejection_reason);
+};
+
+} // namespace bluesky::planning
