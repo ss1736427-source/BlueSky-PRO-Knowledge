16a5399f3bfce44d115bb0923bf1c460d22934a0
ss1736427-source <ss1736427@gmail.com>
2026-09-14 20:46:01 +0300

    EC-05: support deterministic multi-vehicle compilation

--- 04_SOFTWARE/PLANNING/compiler/mission_compiler.hpp
+++ 04_SOFTWARE/PLANNING/compiler/mission_compiler.hpp
@@ -10,14 +10,24 @@ namespace bluesky::planning {
 struct ExecutableMissionPackage {
     std::string package_id;
     std::string vehicle_id;
+    std::string configuration_version;
     std::string package_version;
     std::string mission_id;
     std::string mission_version;
+    std::string route_candidate_id;
+    std::string algorithm_version;
+    std::string model_version;
+    std::string regulatory_plan_id;
+    std::string authorization_id;
     std::vector<std::string> execution_steps;
 };
 
 class MissionCompiler {
 public:
+    static bool compile(const Mission& mission,
+                        std::vector<ExecutableMissionPackage>& packages,
+                        std::string& rejection_reason);
+
     static bool compile(const Mission& mission,
                         ExecutableMissionPackage& package,
                         std::string& rejection_reason);
