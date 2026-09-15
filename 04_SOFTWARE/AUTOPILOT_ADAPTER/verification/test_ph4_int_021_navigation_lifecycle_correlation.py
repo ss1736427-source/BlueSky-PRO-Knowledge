44067f7b8be5fa792731931cba21389ede4084da
ss1736427-source <ss1736427@gmail.com>
2026-09-15 15:37:25 +0300

    PH4-INT-021: expose navigation verification to unittest discovery

---
+++ 04_SOFTWARE/AUTOPILOT_ADAPTER/verification/test_ph4_int_021_navigation_lifecycle_correlation.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from ph4_int_021_navigation_lifecycle_correlation_test import Ph4Int021NavigationLifecycleCorrelationTest
+
+__all__ = ["Ph4Int021NavigationLifecycleCorrelationTest"]
