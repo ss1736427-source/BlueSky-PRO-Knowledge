dacdb446b815b40587e4a4dfc00563ea3fda18d5
ss1736427-source <ss1736427@gmail.com>
2026-09-16 10:48:17 +0300

    PH5-AP-001: implement ArduPilot MAVLink2 reference adapter boundary

---
+++ 04_SOFTWARE/AUTOPILOT_ADAPTER/core/ardupilot_mavlink2_reference_adapter.cpp
@@ -0,0 +1,31 @@
+#include "ardupilot_mavlink2_reference_adapter.hpp"
+
+namespace bluesky::autopilot {
+
+ArduPilotMavlink2ReferenceAdapter::ArduPilotMavlink2ReferenceAdapter(Identity identity)
+    : AutopilotAdapterBaseline(std::move(identity)) {}
+
+Identity ArduPilotMavlink2ReferenceAdapter::getVehicleIdentity() const {
+    auto identity = AutopilotAdapterBaseline::getVehicleIdentity();
+    identity.autopilot = "ArduPilot";
+    identity.protocol = "MAVLINK2";
+    return identity;
+}
+
+std::string ArduPilotMavlink2ReferenceAdapter::getAutopilotIdentity() const {
+    return "ArduPilot";
+}
+
+std::string ArduPilotMavlink2ReferenceAdapter::getProtocolVersion() const {
+    return "MAVLINK2";
+}
+
+Capabilities ArduPilotMavlink2ReferenceAdapter::getCapabilities() const {
+    return {
+        {"arm", "disarm", "takeoff", "land", "hold", "resume", "rtl", "goto", "startMission", "pauseMission", "resumeMission", "abortMission"},
+        {"MISSION_UPLOAD", "MISSION_READBACK", "SEMANTIC_VERIFY"},
+        {"NORMALIZED_TELEMETRY", "FLIGHT_STATE", "LOG_ACQUISITION"}
+    };
+}
+
+} // namespace bluesky::autopilot
