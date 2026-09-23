9c924e2fedd71eeea03a27b0875ecb10921051e7
Sergey <ss1736427@gmail.com>
2026-09-23 13:49:47 +0300

    feat: bind insurance preflight to readiness boundary

---
+++ 04_SOFTWARE/INSURANCE/insurance_readiness_gate.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "insurance_preflight_engine.hpp"
+
+namespace bluesky::insurance {
+
+struct InsuranceReadinessAssessment {
+    bool mandatory_insurance_applicable{false};
+    bool blocks_readiness{false};
+    InsuranceDecision insurance_decision{InsuranceDecision::Review};
+};
+
+class InsuranceReadinessGate {
+public:
+    static InsuranceReadinessAssessment assess(
+        const InsurancePreflightResult& preflight) noexcept {
+        InsuranceReadinessAssessment assessment;
+        assessment.mandatory_insurance_applicable =
+            preflight.minimum_liability_rub > 0.0;
+        assessment.insurance_decision = preflight.decision;
+        assessment.blocks_readiness =
+            assessment.mandatory_insurance_applicable &&
+            preflight.decision == InsuranceDecision::Block;
+        return assessment;
+    }
+};
+
+}  // namespace bluesky::insurance
