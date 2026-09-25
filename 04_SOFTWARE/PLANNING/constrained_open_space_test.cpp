4a686b084310713c987ee1cf61319647a9a993da
Sergey <ss1736427@gmail.com>
2026-09-25 19:59:54 +0300

    test: cover varying altitude envelope

--- 04_SOFTWARE/PLANNING/constrained_open_space_test.cpp
+++ 04_SOFTWARE/PLANNING/constrained_open_space_test.cpp
@@ -7,7 +7,7 @@ int main(){
  auto blocked=ConstrainedOpenSpace::evaluateSegment(e,{{58.995,29.995},{59.005,30.005},100});assert(!blocked.allowed&&blocked.blocking_restriction_ids.size()==1);
  auto open=ConstrainedOpenSpace::evaluateSegment(e,{{58.99,29.99},{58.995,29.995},100});assert(open.allowed);
  z.minimum_altitude_m=150;e.restrictions[0]=z;assert(ConstrainedOpenSpace::evaluateSegment(e,{{58.995,29.995},{59.005,30.005},100}).allowed);
- z.minimum_altitude_m=0;z.maximum_altitude_m=120;e.restrictions[0]=z;assert(!ConstrainedOpenSpace::evaluateSegment(e,{{58.995,29.995},{59.005,30.005},100}).allowed);
+ z.minimum_altitude_m=0;z.maximum_altitude_m=120;e.restrictions[0]=z;assert(!ConstrainedOpenSpace::evaluateSegment(e,{{58.995,29.995},{59.005,30.005},100}).allowed);\n e.restrictions[0]=SpatialRestriction{}; e.restrictions[0]=z; e.restrictions[0].maximum_altitude_m=120; auto envelope=ConstrainedOpenSpace::evaluateSegment(e,{{58.995,29.995},{59.005,30.005},150,80,160}); assert(!envelope.allowed);
  e.complete=false;auto incomplete=ConstrainedOpenSpace::evaluateSegment(e,{{58.995,29.995},{59.005,30.005},100});assert(!incomplete.allowed&&incomplete.blocking_restriction_ids.front()=="ENVIRONMENT_INCOMPLETE");
  e.complete=true;auto a=ConstrainedOpenSpace::evaluateSegment(e,{{58.99,29.99},{58.995,29.995},100});auto b=ConstrainedOpenSpace::evaluateSegment(e,{{58.99,29.99},{58.995,29.995},100});assert(a.dependency_identity==b.dependency_identity);return 0;
 }
